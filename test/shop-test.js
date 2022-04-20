2f8211d259128fff87518cf010ad76b37b89d7e0
evgenu <evgenyyyiii@gmail.com>
2022-04-20 13:47:52 +0300

    Add files via upload

--- test/shop-test.js
+++ test/shop-test.js
@@ -2,30 +2,25 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Owner operations", () => {
-	it("List non-existent item", async () => {
+	let shop;
+	
+	beforeEach(async () => {
 		const Shop = await ethers.getContractFactory("Shop");
-		const shop = await Shop.deploy();
+		shop = await Shop.deploy();
 
 		await shop.deployed();
 
+	});
+
+	it("List non-existent item", async () => {
 		expect(await shop.listItems(1)).to.equal(0);
 	});
 	it("Add item for the first time", async () => {
-		const Shop = await ethers.getContractFactory("Shop");
-		const shop = await Shop.deploy();
-
-		await shop.deployed();
-		
 		await shop.addItem(1, 3);
 		expect(await shop.listItems(1)).to.equal(3);
 	});
 
 	it("Add to present item", async () => {
-		const Shop = await ethers.getContractFactory("Shop");
-		const shop = await Shop.deploy();
-
-		await shop.deployed();
-
 		await shop.addItem(1, 3);
 		await shop.addItem(1, 5);
 		expect(await shop.listItems(1)).to.equal(8);
@@ -33,12 +28,17 @@ describe("Owner operations", () => {
 });
 
 describe("Client operations", () => {
-	it("Buy item", async () => {
-		const [owner, buyer] = await ethers.getSigners();
+	let shop;
+
+	beforeEach(async () => {
 		const Shop = await ethers.getContractFactory("Shop");
-		const shop = await Shop.deploy();
+		shop = await Shop.deploy();
 
 		await shop.deployed();
+	});
+
+	it("Buy item", async () => {
+		const [owner, buyer] = await ethers.getSigners();
 
 		await shop.addItem(1, 3);
 		await shop.connect(buyer).buyItem(1);
@@ -48,10 +48,6 @@ describe("Client operations", () => {
 
 	it("Return Item", async () => {
 		const [owner, buyer] = await ethers.getSigners();
-		const Shop = await ethers.getContractFactory("Shop");
-		const shop = await Shop.deploy();
-
-		await shop.deployed();
 
 		// Under 100 units of block time
 		await shop.addItem(1, 3);
@@ -71,11 +67,7 @@ describe("Client operations", () => {
 
 	it("List all clients", async () => {
 		const [owner, buyer1, buyer2] = await ethers.getSigners();
-		const Shop = await ethers.getContractFactory("Shop");
-		const shop = await Shop.deploy();
-
-		await shop.deployed();
-
+		
 		await shop.addItem(1, 3);
 		await shop.connect(buyer1).buyItem(1);
 		await shop.connect(buyer2).buyItem(1);
